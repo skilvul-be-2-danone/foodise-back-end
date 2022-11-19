3f9b2d359360a18cef454318083a61b95c7f1550
Jasmine Hertiana <jasminehertiana03@gmail.com>
2022-11-19 15:24:07 +0700

    router user

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router()
 const authRoute = require('./auth.router')
 const foodAdminRoute = require('./food.admin.router')
 const foodMemberRoute = require('./food.member.router')
+const userRoute = require('./user.router')
 
 router.use("/api", authRoute)
 router.use("/api", foodAdminRoute)
 router.use("/api", foodMemberRoute)
+router.use("/api", userRoute)
 
 
 module.exports = router;
