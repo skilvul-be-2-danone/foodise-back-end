2e802a602722d3f62d590248a6c7879a38d518d7
Jasmine Hertiana <jasminehertiana03@gmail.com>
2022-11-19 21:17:55 +0700

    cloud database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const database_url = process.env.MONGO_URI || "mongodb://localhost:27017/foodise-be2";
+const database_url = process.env.MONGO_URI || "mongodb+srv://jasminehertiana:jasmine240303@skilvul-back-end-gpa.amlzlfb.mongodb.net/foodise-be2?retryWrites=true&w=majority";
 
 const database = mongoose.connect(database_url)
 
